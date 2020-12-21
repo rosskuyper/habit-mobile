a3c3b6e32ace128f7bc53d52296e1e3b9870b87f
Ross Kuyper <ross@rosskuyper.com>
2020-12-21 20:56:38 +0200

    Add TF secret for slack webhook.

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -7,12 +7,29 @@ data "aws_kms_secrets" "main" {
       service = local.service_name
     }
   }
+
+  secret {
+    name    = "slack_webhook"
+    payload = "AQICAHhOyM5btVbfvpZQ/4Z6nIPAoZ4eCTd0WLRyE39dZQLJxAHVnWCvYVcGJg10bOWubNMbAAAAszCBsAYJKoZIhvcNAQcGoIGiMIGfAgEAMIGZBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDHREuP4t0/okdQyKEwIBEIBs2++51Wh2YjVPzCndcMFAKi3QwzMV+bz7qLYl13AVKgE9qnRho5iUehd0+o8mL8wX1YntoAYNMfeYXnmr3Vyz6a7oUO5ap4LeoW2YhyXEJDxR90G6tBSfG2yZgDU399XQKfU7zLbTIpRTjzsj"
+
+    context = {
+      service = local.service_name
+    }
+  }
 }
 
 resource "aws_ssm_parameter" "expo_password" {
   name      = "/${local.service_name}/expo-password"
   type      = "SecureString"
   value     = data.aws_kms_secrets.main.plaintext["expo_password"]
+  key_id    = data.aws_kms_key.ssm.arn
+  overwrite = "true"
+}
+
+resource "aws_ssm_parameter" "slack_webhook" {
+  name      = "/${local.service_name}/slack-webhook"
+  type      = "SecureString"
+  value     = data.aws_kms_secrets.main.plaintext["slack_webhook"]
   key_id    = data.aws_kms_key.ssm.arn
   overwrite = "true"
 }
