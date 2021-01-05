0fc96268616422ab502b95a3e7c89701fe495774
Ross Kuyper <ross@rosskuyper.com>
2021-01-05 15:42:44 +0200

    Add loading state

--- src/components/ScreenContainer/ScreenContainer.tsx
+++ src/components/ScreenContainer/ScreenContainer.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
-import {SafeAreaView, StyleSheet} from 'react-native'
-import {Layout, LayoutProps} from '@ui-kitten/components'
+import {SafeAreaView, StyleProp, StyleSheet, ViewStyle} from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
   },
-  layout: {
-    flex: 1,
-    marginHorizontal: 24,
-    marginVertical: 16,
-  },
 })
 
-export const ScreenContainer: React.FC<LayoutProps> = ({style, ...layoutProps}) => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Layout style={[styles.layout, style]} {...layoutProps} />
-    </SafeAreaView>
-  )
+type ScreenContainerProps = {
+  style?: StyleProp<ViewStyle>
+}
+
+export const ScreenContainer: React.FC<ScreenContainerProps> = ({children, style}) => {
+  return <SafeAreaView style={[styles.container, style]}>{children}</SafeAreaView>
 }
