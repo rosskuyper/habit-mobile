6defb9231bb091a91b2b934f7597da5a619a79a9
Ross Kuyper <ross@rosskuyper.com>
2021-01-05 12:25:28 +0200

    Move task state to context

--- src/components/TaskGroup/TaskGroup.tsx
+++ src/components/TaskGroup/TaskGroup.tsx
@@ -4,7 +4,6 @@ import {Text} from '@ui-kitten/components'
 import {NewTask, Task} from '../Task/Task'
 import {values} from 'ramda'
 import {TaskGroup as TaskGroupType} from '../../hooks/useTaskState/reducer'
-import {PushTaskHookPayload} from '../../hooks/useTaskState/useTaskState'
 
 const styles = StyleSheet.create({
   container: {
@@ -20,10 +19,9 @@ const styles = StyleSheet.create({
 
 export type TaskGroupProps = {
   group: TaskGroupType
-  pushTask: (payload: PushTaskHookPayload) => void
 }
 
-export const TaskGroup = ({group, pushTask}: TaskGroupProps) => {
+export const TaskGroup = ({group}: TaskGroupProps) => {
   return (
     <View style={styles.container}>
       <Text category="h6" style={styles.title}>
@@ -34,7 +32,7 @@ export const TaskGroup = ({group, pushTask}: TaskGroupProps) => {
         <Task task={task} key={task.id} />
       ))}
 
-      <NewTask key={`${group.id}-new-task`} pushTask={pushTask} taskGroupId={group.id} />
+      <NewTask key={`${group.id}-new-task`} taskGroupId={group.id} />
     </View>
   )
 }
